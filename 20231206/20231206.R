1361ffab18553c091e1087d412340152ca89969c
le402n40\uer <ccannlin@gmail.com>
2023-12-06 16:54:02 +0800

    擷取ptt資訊

--- 20231206/20231206.R
+++ 20231206/20231206.R
@@ -78,3 +78,19 @@ website = read_html(URL)
 
 needed_txt = website %>% html_nodes("tr") %>% html_text()
 needed_txt
+
+
+URL = "https://www.ptt.cc/bbs/AllTogether/index3245.html"
+website = read_html(URL)
+
+needed_html = website %>% html_nodes("a")
+needed_html
+
+needed_txt = needed_html %>% html_text()
+needed_txt
+
+intrested_pos = grep("[徵女]", needed_txt, fixed = TRUE)
+needed_txt[intrested_pos]
+
+needed_link = needed_html[intrested_pos] %>% html_attr("href")
+needed_link
