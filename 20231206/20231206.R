489cf03f0830b47b6536b55f7bb53c7b86890d74
le402n40\uer <ccannlin@gmail.com>
2023-12-06 16:29:45 +0800

    擷取網頁的標題

--- 20231206/20231206.R
+++ 20231206/20231206.R
@@ -3,3 +3,17 @@ URL = "https://reg.ntuh.gov.tw/EmgInfoBoard/NTUHEmgInfo.aspx"
 txt = scan(URL, what = "character", encoding = "UTF-8", quiet = TRUE)
 
 head(txt, 15)
+
+txt_new = paste(txt, sep = "", collapse = " ")
+
+TITLE.pos = gregexpr("<title>.*</title>", txt_new)
+start.TITLE.pos = TITLE.pos[[1]][1]
+end.TITLE.pos = start.TITLE.pos + attr(TITLE.pos[[1]], "match.length")[1] - 1
+
+TITLE.word = substr(txt_new, start.TITLE.pos, end.TITLE.pos)
+
+TITLE.word
+
+TITLE.word = gsub("<title>", "", TITLE.word)
+TITLE.word = gsub("</title>", "", TITLE.word)
+TITLE.word
